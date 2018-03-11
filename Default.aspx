6fbef2853f2d5b9e57a1b7954770a245dd8b5684
Lucio Rogerio SP <lucio.rogerio.senior@gmail.com>
2018-03-11 19:53:24 -0300

    Initial Upload
    http://localhost:50093/Blog/Detail/precisando-exibir-imagens-dinamicamente/057AEC63-B67F-4813-81B6-385621FEAF42

---
+++ Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h1>BASE64 ENCODE - LUCIO ROGERIO DOS SANTOS PINTO</h1>
+    <a href="https://gallery.technet.microsoft.com/Exibir-imagens-diretamente-534c9ced"><asp:Image ID="ImageBase64" runat="server" /></a>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// https://gallery.technet.microsoft.com/Exibir-imagens-diretamente-534c9ced
+/// </summary>
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string strBase64Encode = CreateBase64String(Server.MapPath("./Violão.jpg"));
+
+        ImageBase64.ImageUrl = "data:image/jpeg;base64," + strBase64Encode;
+    }
+
+    private string CreateBase64String(String strCaminho)
+    {
+        // Leitura dos Bytes do Arquivo Imagem
+        byte[] imgByteArray = System.IO.File.ReadAllBytes(strCaminho);
+
+        // Conversão para Base64
+        return Convert.ToBase64String(imgByteArray);
+    }
+}
